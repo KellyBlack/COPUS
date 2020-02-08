3609502ecff8e908b6d965512023819bf7aba75f
KellyBlack <kjblack@gmail.com>
2020-02-08 10:16:11 -0500

    Added comments to the main activity.

--- app/src/main/java/org/cyclismo/copus/MainActivity.kt
+++ app/src/main/java/org/cyclismo/copus/MainActivity.kt
@@ -69,17 +69,22 @@ class MainActivity : AppCompatActivity(),
         fab.setOnClickListener { _ ->
             //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             //    .setAction("Action", null).show()
+
+            // This is for when someone clicks on the email button
+            // on the lower part of the screen.
             decideFileType()
         }
 
+        // Set the default file type. We usually share csv files.
         this.requestFileIntent = Intent(Intent.ACTION_PICK).apply {
             type = "text/csv"
         }
 
+        // Set the value of the variable that holds the value of the start button
         this.startButton = findViewById<Button>(R.id.button)
         if(android.os.Build.VERSION.SDK_INT>android.os.Build.VERSION_CODES.LOLLIPOP) {
             try {
-
+                // If the OS allows, change the colour of the start button so it stands out a bit.
                 val drawing = getDrawable(R.drawable.button_details) //21
                 this.startButton!!.setBackground(drawing)
             } catch (e: RuntimeException) {
@@ -103,20 +108,23 @@ class MainActivity : AppCompatActivity(),
         return when (item.itemId) {
             R.id.action_settings ->
             {
+                // The user chose the option to change the settings.
                 startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
                 true
             }
             R.id.action_about ->
             {
+                // The user chose the option to display the "about" view
                 startActivity(Intent(this@MainActivity, AboutActivity::class.java))
                 true
             }
             R.id.action_help ->
             {
+                // The user chose the option to display the help text.
                 startActivity(Intent(this@MainActivity,HelpActivity::class.java))
                 true
             }
-            else -> super.onOptionsItemSelected(item)
+            else -> super.onOptionsItemSelected(item) // Nothing for me to do here....
         }
     }
 
@@ -124,41 +132,61 @@ class MainActivity : AppCompatActivity(),
     @Suppress("UNUSED_PARAMETER")
     public fun clickCOPUSurl(view: View)
     {
+        // If someone clicks on the about screen open up the URL to the
+        // COPUS web page so they can get more details.
         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.COPUS_URL)))
         startActivity(browserIntent)
     }
 
     override fun onDeleteObservationAndProceed(dialog: DialogFragment?)
     {
+        // Someone hit the start button and confirmed they want to delete the
+        // unsaved observation. Start this bad boy up!
+        // Change the button, reset any stray text, and start the timer.
         val view = findViewById<Button>(R.id.button)
         val textLabel = findViewById<TextView>(R.id.timerTextLabel)
         val counter: Chronometer = findViewById<Chronometer>(R.id.TimeView)
         val observationFragment = supportFragmentManager.findFragmentById(R.id.observationFragment) as ClassActions
 
+        // Let the object keeping track of view to the current observation know we are
+        // starting over. Make everything blank and change the start button.
         observationFragment.startNewObservation()
         view.text = getString(R.string.Timer_End)
         textLabel.text = ""
         counter.format = getString(R.string.Time_Stamp_Label)
+
+        // The timer react object will keep track of the last time the timer rolled over to the next
+        // two minute time, and the base is used to check how long that has happened. This needs to
+        // be updated regularly. Then start this thing.
         counter.base = SystemClock.elapsedRealtime()
         counter.start()
+
+        // Let the button know it is time to go.
         //counter.onChronometerTickListener = object: TimerReact
         counter.setOnChronometerTickListener(TimerReact(this))
+
+        // Make sure the screen does not lock up in the middle of an observation or
+        // just before making an observation.
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
     }
 
     override fun onDeleteObservationCanel(dialog : DialogFragment?)
     {
-        // User clicked on the cancel button. Haco nada
+        // User clicked on the cancel button. Nothing to do????
     }
 
     override fun onStopTimerNoticeCancel(dialog : DialogFragment?)
     {
-        // user clicked on the cancel button on the end timer button.
+        // user clicked on the cancel button on the end timer button. Nothing to do???
     }
 
     public fun pushCurrentState(period:Long = -1)
     {
+        // The timer has rolled over to the next two minute period.
+        // Save all the information, update the text labels, and
+        // let the view to the menu options know to start over.
+
         val textLabel = findViewById<TextView>(R.id.timerTextLabel)
         val observationFragment = supportFragmentManager.findFragmentById(R.id.observationFragment) as ClassActions
         val periodText = "(${period})"
@@ -171,22 +199,36 @@ class MainActivity : AppCompatActivity(),
 
     public fun startButton(view: View)
     {
+        /*
+            This is the function that starts the main action. It is called whenever someone
+            clicks on the start button. The state of the system is defined by the text on
+            the start button. If it says "start" then it means the timer was off, and the
+            user needs to confirm that she really wants to start the timer. Otherwise it
+            is time to bring everything to a close.
+         */
+
         if(view is Button)
         {
+            // The thing pressed was a button. Find the timer view and the fragment that has the
+            // information of the current set of observations.
             val counter: Chronometer = findViewById(R.id.TimeView)
             val observationFragment = supportFragmentManager.findFragmentById(R.id.observationFragment) as ClassActions
 
             if (view.text == getString(R.string.Timer_Start))
             {
+                // The timer needs to be started.
 
                 if(observationFragment.numberObservations() > 0)
                 {
+                    // We have an old set of data. Double check to make sure we really
+                    // want to start this thing.
                     val confirmDelete = ConfirmDeleteCurrentObservation()
                     val fragmentManager = supportFragmentManager
                     confirmDelete.show(fragmentManager, "deleteCurrentObservation")
                 }
                 else
                 {
+                    // There is no old data. Just start it and get going.
                     onDeleteObservationAndProceed(null)
                 }
 
@@ -194,15 +236,22 @@ class MainActivity : AppCompatActivity(),
             }
             else
             {
+                // We need to stop the timer.
+
                 if(!observationFragment.isClear())
                 {
+                    // We have unsaved information. Save it to the list of observations.
                     observationFragment.pushCurrentState()
                 }
+
+                // Reset the buttons and labels and such. Clear the display and turn off
+                // the flag that stops the screen saver from blanking the screen.
                 view.text = getString(R.string.Timer_Start)
                 counter.stop()
                 observationFragment.clearAllCheckboxes()
                 window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+                // Stop the timer and let the user know that nothing has been saved yet.
                 val stopTimerNotice = StopTimerDialog()
                 val fragmentManager = supportFragmentManager
                 stopTimerNotice.show(fragmentManager,"noticeTimerEnd")
@@ -212,16 +261,23 @@ class MainActivity : AppCompatActivity(),
 
     override fun onPrintTableCSV(dialog: DialogFragment?)
     {
+        // Called in response to the user hitting the email dialog.
+        // The user wants to email the csv file as a table.
         sendResults(ClassActions.FileType.TABLE)
     }
 
     override fun onPrintFlatCSV(dialog : DialogFragment?)
     {
+        // Called in response to the user hitting the email dialog.
+        // The user wants to email the csv file as a flat file.
         sendResults(ClassActions.FileType.FLAT)
     }
 
     private fun decideFileType()
     {
+        // The user hit the email button. We need to figure out whether the
+        // person wants to send a table or a flat file. Put up a dialog with
+        // the options.
         val decideFileType = DecideTableOrFlatCSV()
         val fragmentManager = supportFragmentManager
         decideFileType.show(fragmentManager, "decideFileType")
@@ -230,12 +286,22 @@ class MainActivity : AppCompatActivity(),
     public fun sendResults(which : ClassActions.FileType)
     {
 
+        // The user hit the email button and responded to the window asking what kind of file
+        // they want to send. We just need to collect the data, create a file, and then
+        // request that the email application pass the information along.
+
+        // Get the fragment that keeps track of the user input. Save the
+        // results in one string that has the information in the necessary
+        // format.
         val observationFragment = supportFragmentManager.findFragmentById(R.id.observationFragment) as ClassActions
         val allObservations : String = observationFragment.observationsAsString(which)
 
+        // Save the information to a file.
         val fileToWrite : File? = saveStringAsFile(allObservations)
         if(fileToWrite!=null)
         {
+            // The file has been saved. Now call up the email application and
+            // make sure it knows the file to include as an attachment.
             val settings  = PreferenceManager.getDefaultSharedPreferences(this)
             val toAddress : String = settings.getString("defaultEmailTo","") ?: ""
             val email = Intent(Intent.ACTION_SEND)
@@ -262,6 +328,8 @@ class MainActivity : AppCompatActivity(),
             }
             catch (e : IllegalArgumentException)
             {
+                // oops! Could not get the file for some reason. Just walk away...
+                // TODO - really should be more graceful about this, ya know!
                 //println(e)
             }
         }
@@ -270,6 +338,8 @@ class MainActivity : AppCompatActivity(),
 
     private fun saveStringAsFile(contents:String) : File
     {
+        // Take the set of observations as a string and save them to a file.
+        // Return a File object that points to the file just created.
         val directoryFile: File = File(filesDir,"observations")
         directoryFile.mkdirs()
 
